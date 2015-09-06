1006341419630bb1c61a5a9a8f334148fec17640
Kevin Naquin <kevinnaquin@Kevins-MacBook-Pro.local>
2015-09-06 17:32:31 -0400

    Added support for handle image downloads

--- plugins/httpBuilder/grails-app/services/httpbuilder/HttpDownloadService.groovy
+++ plugins/httpBuilder/grails-app/services/httpbuilder/HttpDownloadService.groovy
@@ -2,7 +2,9 @@ package httpbuilder
 
 
 import grails.transaction.Transactional
+import groovyx.net.http.ContentType
 import groovyx.net.http.HTTPBuilder
+import static groovyx.net.http.Method.*
 import static groovyx.net.http.Method.GET
 
 
@@ -13,10 +15,15 @@ class HttpDownloadService {
 		def http = new HTTPBuilder(params.url)
 
 		http.request(GET) { req ->
-			response.success = { resp, reader ->
+			response.success = { 
+				response, reader ->
+
+				def contentType = response.allHeaders.find({ it.name =~ /(?i)Content-Type/})
+				if (contentType) { contentType = contentType.value }
 
 
-				return reader
+				if (contentType.contains("image/jpeg") || contentType.contains("image/png")) { return reader.bytes }
+				else { return reader }
 			}
 		}
 	}
