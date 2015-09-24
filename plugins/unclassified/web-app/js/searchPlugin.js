1c67bc6956b1cbcd30c5edbb5da19441d041e56a
Kevin Naquin <kevinnaquin@Kevins-MacBook-Pro.local>
2015-09-23 22:42:49 -0400

    Changed the plugin name to be more generic

--- plugins/unclassified/web-app/js/searchPlugin.js
+++
@@ -1,9 +0,0 @@
-var initializeLibraryCheckboxesPlugin = initializeLibraryCheckboxes;
-initializeLibraryCheckboxes = function() {
-	if (tlv.libraries) { initializeLibraryCheckboxesPlugin(); }
-	else {
-		var library = tlv.availableResources.libraries[0];
-		var libraryCheckbox = $("#searchTabLibrary" + library.capitalize() + "Checkbox");
-		libraryCheckbox.trigger("click");
-	}
-}
